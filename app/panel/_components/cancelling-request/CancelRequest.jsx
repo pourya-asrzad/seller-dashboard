62168f9057c6438da4e59f24be2c530a81e6a993
pourya-asrzad <poriaasrzad@gmail.com>
2025-05-20 17:50:43 +0330

    update CancelRequest component to enhance heading styling with bold font

--- app/panel/_components/cancelling-request/CancelRequest.jsx
+++ app/panel/_components/cancelling-request/CancelRequest.jsx
@@ -3,7 +3,7 @@ const { Button, Divider } = require("@heroui/react");
 export const CancelRequest = () => {
   return (
     <div>
-      <h2>درخواست فوری تسویه</h2>
+      <h2 className="font-bold mb-6">درخواست فوری تسویه</h2>
       <div className="  max-w-[416px] flex flex-col justify-center rounded-lg w-full h-[218px]  px-4  bg-primary-300 gap-8  bg-[url('/images/Card-Mask.png')] bg-cover bg-center">
         <div className="flex justify-between items-center">
           <span className="text-sm ">موجودی</span>
